2c5f5594e4678633698d256dba3de20da5fad18d
SidGames5 <saytleader@outlook.com>
2023-12-13 15:15:03 -0500

    day 2

---
+++ day2.cxx
@@ -0,0 +1,22 @@
+// left off at 31:43
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "John";
+    int age = 40;
+
+    cout << "There once was a man named " << name << endl;
+    cout << "He was " << age << " years old" << endl;
+    cout << "He liked the name " << name << endl;
+    cout << "But did not like being " << age << endl;
+
+    age = 41;
+
+    cout << "Next year " << name << " will turn " << age << " years old" << endl;
+
+    return 0;
+}
