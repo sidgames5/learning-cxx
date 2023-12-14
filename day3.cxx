8056e952e55c63b935d59824e2d581682af69616
SidGames5 <saytleader@outlook.com>
2023-12-14 17:37:44 -0500

    day 3

---
+++ day3.cxx
@@ -0,0 +1,16 @@
+// left off at 39:15
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string subject = "Math";
+    int percent = 96;
+    double gpa = 3.84;
+    bool passing = true;
+
+    return 0;
+}
