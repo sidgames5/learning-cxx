f0da814f87e4f8bb68905f7bcdbdef59a7bff402
SidGames5 <saytleader@outlook.com>
2023-12-16 11:19:28 -0500

    day 5

---
+++ day5.cxx
@@ -0,0 +1,27 @@
+// left off at 59:41
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 5;
+    double num2 = 5.5;
+    cout << 10.0 / 3 << endl;
+    cout << 10 / 3 << endl;
+    cout << 10 % 3 << endl;
+    cout << num1 / num2 << endl;
+    num1++;
+    cout << num1 << endl;
+    num1--;
+    cout << num1 << endl;
+    num1 += 5;
+    cout << num1 << endl;
+    cout << pow(2, 3) << endl;
+    cout << sqrt(36) << endl;
+    cout << fmax(10, 11) << endl;
+
+    return 0;
+}
