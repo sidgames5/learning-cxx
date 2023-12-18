d881a373e982788acef0e965536d1f490f50f5d6
SidGames5 <saytleader@outlook.com>
2023-12-18 18:00:08 -0500

    day 7

---
+++ day7.cxx
@@ -0,0 +1,20 @@
+// left off at 1:09:28
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << num1 + num2 << endl;
+
+    return 0;
+}
