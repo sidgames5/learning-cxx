69f7f90fcb3a00322b4d9ae19ddb2408b806b53f
SidGames5 <saytleader@outlook.com>
2023-12-26 10:53:22 -0500

    day 17

---
+++ day17.cxx
@@ -0,0 +1,21 @@
+// left off at 2:39:32
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+    }
+
+    int nums[] = {1, 2, 5, 6};
+    for (int i = 0; i < 4; i++)
+    {
+        cout << nums[i] << endl;
+    }
+
+    return 0;
+}
